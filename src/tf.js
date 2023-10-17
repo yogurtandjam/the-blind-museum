383da034bc3ba84d983d389f3f4782df31b0f0ec
Jeremy Lee <calijeremylee@gmail.com>
2023-10-17 10:51:27 -0500

    add prettier. some really minor cleanup

--- src/tf.js
+++ src/tf.js
@@ -5,21 +5,20 @@ const EAR_THRESHOLD = 0.2;
 let detector;
 const videl = document.getElementById("webcam");
 
-// TODO: use these. maybe add typescript too so this is more clear
 const rightEyeKeyPoints = (keypoints) => {
   return {
-    lower: keypoints[145],
-    upper: keypoints[159],
-    left: keypoints[33],
-    right: keypoints[133],
+    bottomLandmark: keypoints[145],
+    topLandmark: keypoints[159],
+    leftLandmark: keypoints[33],
+    rightLandmark: keypoints[133],
   };
 };
 const leftEyeKeyPoints = (keypoints) => {
   return {
-    lower: keypoints[374],
-    upper: keypoints[386],
-    left: keypoints[362],
-    right: keypoints[263],
+    bottomLandmark: keypoints[374],
+    topLandmark: keypoints[386],
+    leftLandmark: keypoints[362],
+    rightLandmark: keypoints[263],
   };
 };
 
@@ -40,25 +39,9 @@ export const detectEyes = async (cb) => {
   if (!detector) return;
   const faces = await detector.estimateFaces(videl);
   if (faces && faces.length > 0) {
-    faces.forEach((prediction) => {
-      // Right eye parameters
-      const lowerRight = prediction.keypoints[145];
-      const upperRight = prediction.keypoints[159];
-      const leftRight = prediction.keypoints[33];
-      const rightRight = prediction.keypoints[133];
-      const rightEAR = calculateEAR(
-        upperRight,
-        lowerRight,
-        leftRight,
-        rightRight,
-      );
-
-      // Left eye parameters
-      const lowerLeft = prediction.keypoints[374];
-      const upperLeft = prediction.keypoints[386];
-      const leftLeft = prediction.keypoints[362];
-      const rightLeft = prediction.keypoints[263];
-      const leftEAR = calculateEAR(upperLeft, lowerLeft, leftLeft, rightLeft);
+    faces.forEach(({ keypoints }) => {
+      const rightEAR = calculateEAR(rightEyeKeyPoints(keypoints));
+      const leftEAR = calculateEAR(leftEyeKeyPoints(keypoints));
 
       // True if the eye is closed
       const closed = leftEAR <= EAR_THRESHOLD && rightEAR <= EAR_THRESHOLD;
@@ -72,12 +55,12 @@ function euclideanDistance(point1, point2) {
   return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
 }
 
-function calculateEAR(
+function calculateEAR({
   topLandmark,
   bottomLandmark,
   leftLandmark,
   rightLandmark,
-) {
+}) {
   const verticalDist = euclideanDistance(topLandmark, bottomLandmark);
   const horizontalDist = euclideanDistance(leftLandmark, rightLandmark);
   const ear = verticalDist / horizontalDist;
