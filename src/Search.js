bb50a6b603737f6ea11fc68148db4b4984d4a4da
Filip Spiridonov <filip.spiridonov@gmail.com>
2023-10-24 12:55:25 -0400

    Filter art with images. Refactor search. Fix react warnings (#34)

--- src/Search.js
+++ src/Search.js
@@ -1,40 +1,49 @@
 import debounce from "lodash.debounce";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { OBJ_URL, SEARCH_URL } from "./consts";
 import { Art } from "./models/Art";
 import Image from "./Image";
+
+const search = async (q) => {
+  const res = await fetch(`${SEARCH_URL}?q=${q}`);
+  const artworkIds = await res.json();
+
+  console.log({ artworkIds });
+
+  const responses = await Promise.all(
+    (artworkIds.objectIDs || [])
+      .slice(0, 10)
+      .map((id) => fetch(`${OBJ_URL}/${id}`).then((res) => res.json()))
+  );
+
+  const validObjs = responses.filter(
+    (res) => res.objectID && res.primaryImageSmall
+  );
+  return validObjs.map((validObj) => new Art(validObj));
+};
 
 export const Search = ({ eyesClosed }) => {
   const [query, setQuery] = useState("");
   const [art, setArt] = useState([]);
 
-  const search = (q) => {
-    fetch(`${SEARCH_URL}?q=${q}`)
-      .then((res) => res.json())
-      .then((artworkIds) => {
-        console.log(artworkIds);
-        Promise.all(
-          (artworkIds.objectIDs || []).slice(0, 10).map((id) => {
-            return fetch(`${OBJ_URL}/${id}`).then((res) => res.json());
-          }),
-        )
-          .then((responses) => responses.filter((res) => res.objectID))
-          .then((validObjs) => validObjs.map((validObj) => new Art(validObj)))
-          .then(setArt);
-      });
-  };
-  const debouncedFetch = useMemo(() => debounce(search, 500), []);
+  const debouncedFetch = useCallback(
+    debounce((q) => search(q).then(setArt), 500),
+    []
+  );
+
   useEffect(() => {
-    debouncedFetch(query);
+    if (query.length > 0) {
+      debouncedFetch(query);
+    }
   }, [query]);
 
   useEffect(() => {
-    console.log(art);
+    console.log({ art });
   }, [art]);
   const handleChangeQuery = (e) => setQuery(e.target.value);
   return (
     <div>
-      <label for="search">Search for an art piece:</label>
+      <label htmlFor="search">Search for an art piece:</label>
       <input
         id="search"
         value={query}
@@ -49,7 +58,7 @@ export const Search = ({ eyesClosed }) => {
       )}
       <div style={{ visibility: eyesClosed ? "visible" : "hidden" }}>
         {art.map((artPiece) => (
-          <div>
+          <div key={artPiece.objectID}>
             <Image src={artPiece.primaryImageSmall} />
           </div>
         ))}
