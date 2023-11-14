97fd9ad5e9e77cbe43cf3a79619cb970c9ce4acc
Filip Spiridonov <filip.spiridonov@gmail.com>
2023-11-13 23:09:34 -0500

    Improve UI styling using Baseweb components

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,14 +1,22 @@
+import { useStyletron } from "baseui";
+
 type ImageProps = {
   src: string;
 };
 export default (props: ImageProps) => {
+  const [css, theme] = useStyletron();
   return (
-    <div>
+    <div
+      className={css({
+        marginTop: theme.sizing.scale650,
+      })}
+    >
       <img
         src={props.src}
-        style={{
-          height: "500px",
-        }}
+        className={css({
+          maxWidth: "100%",
+          maxHeight: "100%",
+        })}
       />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AppProviders } from "./AppProviders";
 import reportWebVitals from "./reportWebVitals";
 
 const root = createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>
 );
 
